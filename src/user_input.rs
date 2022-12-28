256df0bb0b5a01a754866ed1672a7ad9a10daca2
Guy Marshall <guymarshall9@gmail.com>
2022-12-28 21:06:00 +0000

    Change user_input to f64

--- src/user_input.rs
+++ src/user_input.rs
@@ -1,13 +1,13 @@
 use std::io;
 
-pub fn get_user_input(prompt: &str) -> i32 {
+pub fn get_user_input(prompt: &str) -> f64 {
     println!("{}", prompt);
 
     let mut user_input: String = String::new();
 
     io::stdin().read_line(&mut user_input).expect("Failed to read line");
 
-    let number: i32 = user_input.trim().parse().expect("Please enter an integer!");
+    let number: f64 = user_input.trim().parse().expect("Please enter an integer!");
 
     return number;
 }
